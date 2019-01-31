f76bd7f086ba3d2fe166658718e6e8406c476551
PaulM <pmeehan456@gmail.com>
2019-01-30 21:48:10 -0500

    final version for HW7

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# RPS-Multiplayer
+# Rock-Paper-Scissors
+
+### Programmer: Paul Meehan
+### Date: 1/30/2019
+
+**Purpose:**
+This application provides a digital version of the classic Rock-Paper-Scissors game.  It is intended to be played by two players, each with the game open in separate windows.
+
+**User instructions:**
+Two players each open the game from their own computer.  The game will give you instructions in the middle of the display
+
+Sign into the game by entering your name and clicking the Submit button.  Once two players have signed in, the game will begin.
+
+For each round, each play selects a choice of Rock, Paper or Scissors.  The object of the game is to select the choice that beats the other players choice.  But choose wisely!  Each choice is capable of beating another choice, but will also be beaten by one other choice.  The ranking goes as follows:
+
+* Paper beats Rock
+* Rock beats Scissors
+* Scissors beats Paper
+
+After each player has made a choice, the winner will be displayed and the scores will be updated.  There is no limit to the number of rounds you can play.
+
+If you leave the game and come back later, your scores will pick up right where you left off.
+
+You can start a whole new game by clicking the New Game button.
+
+
+**Technical information:**
+The application makes use of a Firebase database.  This database is available to the public without user authentication.
+
--- assets/javascript/rps-multiplayer.js
+++ assets/javascript/rps-multiplayer.js
@@ -9,23 +9,23 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-console.log("database initalized");
 
-var v_p1name = "";
-var v_p1pick = "";
-var v_p1wins = 0;
-var v_p1losses = 0;
-var v_p1ties = 0;
-var v_p2name = "";
-var v_p2pick = "";
-var v_p2wins = 0;
-var v_p2losses = 0;
-var v_p2ties = 0;
-var v_conversation = "";
-var v_message = "";
+// Define global variables
+var v_p1name = "";   // Player 1 name
+var v_p1pick = "";   // Option chosen by player 1
+var v_p1wins = 0;    // Number of times player 1 won
+var v_p1losses = 0;  // Number of times player 1 lost
+var v_p1ties = 0;    // Number of times player 1 tied
+var v_p2name = "";   // Player 2 name
+var v_p2pick = "";   // Option chosen by player 2
+var v_p2wins = 0;    // Number of times player 2 won
+var v_p2losses = 0;  // Number of times player 2 lost
+var v_p2ties = 0;    // Number of times player 2 tied
+var v_conversation = "";  // Full conversation chatted between the two players (in HTML format)
+var v_message = "";  // Message to both players
 
+// Write values in global variables out to the database
 function updateDatabase() {
-    console.log("updateDatabase");
     database.ref().set({    
         p1name: v_p1name,
         p1pick: v_p1pick,
@@ -43,6 +43,7 @@ function updateDatabase() {
 
 };
 
+// Start a new game - reset all global variables and prompt for player names
 $("#newGame").on("click", function () {
     event.preventDefault();
     v_p1name = "";
@@ -57,45 +58,62 @@ $("#newGame").on("click", function () {
     v_p2ties = 0;
     v_conversation = "";
     v_message = "Waiting for Player 1 to sign in ..."
+    // Display text box prompt for palyer names and submit button
     $("#enterPlayers").attr("hidden", false);
+    // Hide the New Game button
     $("#newGame").attr("hidden", true);
+    // Hide the game choice buttons
     $(".game").css("visibility", "hidden");
     updateDatabase();
 
 });
 
+// Reset the display for the next round.
 function resetGame() {
-    console.log("resetGame");
+    // Hide what was chosen during last round
     $(".choice").attr("hidden", true);
+    // Display the game choice buttons
     $(".game").attr("hidden", false);
     $(".game").css("visibility", "visible");
+    // Update the player instructions.
     v_message = "Make your choices ...";
     updateDatabase();
 };
 
+// Sign in new player and update the display
 $("#signin").on("click", function() {
     event.preventDefault();
+    // Obtain the name that is entered
     newName = $("#pname").val();
+    // If player 1 name is blank, make new entry player 1.
     if (v_p1name.length === 0) {
         v_p1name = newName;
+        // Display button to accept chats from Player 1
         $("#submit1").attr("hidden",false);
         updateDatabase();
+    // If player 1 already recorded, make new entry player 2
+    // and do not allow more players to join.
     } else {
         v_p2name = newName;
         updateDatabase();
+        // Hide the text box and button for adding new players.
         $("#enterPlayers").attr("hidden", true);
+        // Display the button to allow starting a new game.
         $("#newGame").attr("hidden", false);
+        // Display button to accept chats from Player 2
         $("#submit2").attr("hidden", false);
+        // Set the display up for the first round.
         resetGame();
     };
     $("#pname").val("");
 });
 
+// Determine which player won the game.
 function determineWinner () {
-    console.log("determineWinner");
-    var winner = "";
-    var intervalID = 0;
+    var winner = "";       // Flag indicating which player won or tie game
+    var intervalID = 0;    // Timer interval id.
 
+    // Determine winner based on each possible combination of choices.
     switch (v_p1pick) {
         case "rock":
             switch (v_p2pick) {
@@ -109,6 +127,7 @@ function determineWinner () {
                     winner = "p1";
                     break;
             };
+            // Set the image for Player 1 choice.
             $("#image1").attr("src","assets/images/rock.jpg")
             break;
         case "paper":
@@ -123,6 +142,7 @@ function determineWinner () {
                     winner = "p2";
                     break;
             };
+            // Set the image for Player 1 choice.
             $("#image1").attr("src", "assets/images/paper.jpg")
             break;
         case "scissors":
@@ -137,10 +157,12 @@ function determineWinner () {
                     winner = "tie";
                     break;
             };
+            // Set the image for Player 1 choice.
             $("#image1").attr("src", "assets/images/scissors.jpg")
         break;
     };
 
+    // Set the image for Player 2 choice.
     switch (v_p2pick) {
         case "rock":
             $("#image2").attr("src", "assets/images/rock.jpg")
@@ -153,6 +175,8 @@ function determineWinner () {
             break;
     };
 
+    // Update each player's scores according to who won
+    // and update the common message to both players.
     if (winner === "p1") {
         v_p1wins = v_p1wins + 1;
         v_p2losses = v_p2losses + 1;
@@ -167,18 +191,21 @@ function determineWinner () {
         v_message = "Tie Game!!";
     };
 
+    // Reset the variables with each player's choices.
     v_p1pick = "";
     v_p2pick = "";
 
     updateDatabase();
 
+    // Pause for 5 seconds before resetting the game for the next round.
     intervalID = setTimeout(resetGame, 5000);
 
 };
 
 
+// Procedure run each time database is updated.
 database.ref().on("value", function (snapshot) {
-    console.log("database ref value");
+    // Set global variables to values from the database.
     v_p1name = snapshot.val().p1name;
     v_p1pick = snapshot.val().p1pick;
     v_p1wins = snapshot.val().p1wins;
@@ -192,6 +219,7 @@ database.ref().on("value", function (snapshot) {
     v_conversation = snapshot.val().conversation;
     v_message = snapshot.val().message;
 
+    // Display the values in the appropriate places in the document.
     $("#d_p1name").text(v_p1name);
     $("#d_p1wins").text(v_p1wins);
     $("#d_p1losses").text(v_p1losses);
@@ -202,49 +230,67 @@ database.ref().on("value", function (snapshot) {
     $("#d_p2ties").text(v_p2ties);
     $("#messages").text(v_message);
 
+    // Display the chat conversation in HTML
     $("#conversation").html(v_conversation);
+    // The conversation Div is returned as an array with one item.
     var convoDiv = $("#conversation")[0];
+    // Scroll to the bottom of the conversation to ensure most recent statements are displayed
     convoDiv.scrollTop = convoDiv.scrollHeight;
 
+    // If the database contains a choice from both players, do the following:
     if (v_p1pick.length > 0 && v_p2pick.length > 0) {
+        // Hide the game choice buttons
         $("#p1game").attr("hidden",true);
+        // Display the image and text of the player's choice
         $("#p1choice").attr("hidden",false);
         $("#choice1Text").text(v_p1pick);
+
         $("#p2game").attr("hidden", true);
         $("#p2choice").attr("hidden", false);
         $("#choice2Text").text(v_p2pick);
 
+        // Determine who won the round.
         determineWinner();
     }
 
+    // If no name entered yet for Player 1 or Player 2, do the following:
     if (v_p1name.length === 0 || v_p2name.length === 0) {
+        // Display common message indicating which player needs to sign in.
         if (v_p1name.length === 0) {
             v_message = "Waiting for Player 1 to sign in ...";
         } else {
             v_message = "Waiting for Player 2 to sign in ...";
         };
+        // Display text box and button to allow players to sign in
         $("#enterPlayers").attr("hidden", false);
+        // Hide button for starting a new game.
         $("#newGame").attr("hidden", true);
         $(".game").css("visibility", "hidden");
         updateDatabase();
     } else {
+        // If both players are signed in:
+        // Hide text box and button to not allow more players to sign in
         $("#enterPlayers").attr("hidden", true);
+        // Display button to allow starting a new game.
         $("#newGame").attr("hidden", false);
         $(".game").css("visibility", "visible");
     };
 });
 
-
+// Function to process player clicking game choice button.
 $(".gameButton").on("click", function() {
-    console.log("gameButton");
+    // Determine the ID of the button that was clicked
     var thisButton = $(this).attr("id");
-    console.log(thisButton);
+    // The player number is the first two characters of the id.
     var thisPlayer = thisButton.slice(0,2);
+    // The choice made is the remainder of the id.
     var thisPick = thisButton.slice(2);
 
+    // Display commen message that game is waiting on the "other" player to choose.
     switch (thisPlayer) {
         case "p1":
             v_p1pick = thisPick;
+            // Hide the player's game choice buttons.
             $("#p1game").css("visibility","hidden");
             v_message = "Waiting for " + v_p2name + " to choose...";
             break;
@@ -259,17 +305,21 @@ $(".gameButton").on("click", function() {
 
 });
 
-
+// Record the chat text from Player 1.
 $("#submit1").on("click", function () {
     var pname = v_p1name;
+    // chat text is added as a new Div that is color-coded and justified based on the player.
     var newP = "<div class='chatLeft'>";
     newP = newP + pname + ": " + $("#chat-input").val();
     newP = newP + "</div>";
+    // Append the new Div to the current conversation.
     v_conversation = v_conversation + newP;
+    // Reset the text in the chat box.
     $("#chat-input").val("");
     updateDatabase();
 });
 
+// Record the chat text from Player 2.
 $("#submit2").on("click", function () {
     var pname = v_p2name;
     var newP = "<div class='chatRight'>";
@@ -281,10 +331,8 @@ $("#submit2").on("click", function () {
 });
 
 
-// Start the game with the new game button displayed if players are already in the database.
-// Otherwise, display the prompt for player names.
-
-console.log("here");
+// When game first starts, sign in text box and button will be displayed and new game button is hidden.
+// This will be immediately reversed if database shows players are already signed in. 
 $("#enterPlayers").attr("hidden", false);
 $("#newGame").attr("hidden", true);
 $("#submit1").attr("hidden", true);
@@ -292,4 +340,3 @@ $("#submit2").attr("hidden", true);
 
 $(".choice").attr("hidden", true);
 
-console.log("Bottom");
